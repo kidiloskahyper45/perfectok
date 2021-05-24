98a8e342981c99cb7ea4a5003beefd1a8acc9379
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-05-25 02:01:18 +0530

    Update karma.py

--- YoneRobot/modules/karma.py
+++ YoneRobot/modules/karma.py
@@ -3,7 +3,7 @@ from typing import Dict, Union
 from pyrogram import filters
 
 from YoneRobot.db.mongo_helpers.karma import is_karma_on, karma_off, karma_on
-from YoneRobot.pyrogramee.pluginhelper import member_permissions
+from YoneRobot.pyrogramee.pluginshelper import member_permissions
 from YoneRobot.mongo2 import db
 from YoneRobot.services.pyrogram import pbot as app
 
