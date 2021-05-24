36279333b8aa901433fd0db6c6a7b914350814d7
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-05-25 01:21:26 +0530

    Update karma.py

--- YoneRobot/modules/karma.py
+++ YoneRobot/modules/karma.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 
 from YoneRobot.db.mongo_helpers.karma import is_karma_on, karma_off, karma_on
 from YoneRobot.pyrogramee.pluginhelper import member_permissions
-from YoneRobot.mongo import db
+from YoneRobot.mongo2 import db
 from YoneRobot.services.pyrogram import pbot as app
 
 karmadb = db.karma
