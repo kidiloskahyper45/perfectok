05a151c2b123c45299635907f98e6456cb9e5ce9
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2021-05-24 22:00:27 +0530

    Update __init__.py

--- YoneRobot/__init__.py
+++ YoneRobot/__init__.py
@@ -5,6 +5,7 @@ import time
 import spamwatch
 
 import telegram.ext as tg
+from aiohttp import ClientSession
 from pyrogram import Client, errors
 from telethon import TelegramClient
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
